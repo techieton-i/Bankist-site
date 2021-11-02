9178af942a42e709cdded44613684ba5d0a26ee0
techieton-i <odejobiakin@gmail.com>
2021-11-02 15:17:44 +0100

    Bankist website

--- script.js
+++ script.js
@@ -53,3 +53,85 @@ btnScrollTo.addEventListener('click', function (e) {
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+
+// PAGE NAVIGATION
+
+// using normal method
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// using events delegation
+// 1. you select a common parent and put the event listener on it
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));     old method
+
+// using events delegation
+
+tabsContainer.addEventListener('click', function (e) {
+
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // active tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  // activate content area
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+  document.querySelector(`.operations__content--${clicked.getAttribute('data-tab')}`)
+    // or use clicked.dataset.tab
+    .classList.add('operations__content--active');
+});;
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////
+
+
+
+// Event Propagation capturing and bubbling
+// const randomint = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+// const randomColor = () => `rgb(${randomint(0, 255)},${randomint(0, 255)},${randomint(0, 255)})`;
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   e.preventDefault();
+//   this.style.backgroundColor = randomColor();
+// });
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   e.preventDefault();
+//   this.style.backgroundColor = randomColor();
+// });
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   e.preventDefault();
+//   this.style.backgroundColor = randomColor();
+// });
